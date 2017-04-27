a17ad326535b0d85c52cc3b7c604553929c8a598
Calvin Cushman and Karina Roush <calvin@>
2017-04-27 11:50:54 -0700

    add rules to highlight specific ride lists in css and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(function() {
   var height = parseInt(prompt("Enter your height in inches:"));
+  $(".rides").hide();
 
   if (height >= 52) {
-    $('.adultRides').show();
+    $('.rides').show()
+    $('.adultRides').addClass("yellow-background")
 
   } else {
-    $('.kidRides').show();
-  }
+    $('.rides').show()
+    $('.kidRides').addClass("yellow-background")
+  };
 
 });
